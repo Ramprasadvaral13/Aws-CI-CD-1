1564b5e6d5b8eff936854d149e2d056cc54872fe
Ramprasadvaral13 <136104228+Ramprasadvaral13@users.noreply.github.com>
2023-09-14 20:04:55 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull ramprasadv7/python-flask:latest
+docker pull ramprasadv7/python-flask
 
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 ramprasadv7/python-flask:latest
+docker run -d -p 5000:5000 ramprasadv7/python-flask
