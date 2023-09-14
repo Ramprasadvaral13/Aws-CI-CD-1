d25304705381c546a83780f35e85e3988c469e69
Ramprasadvaral13 <136104228+Ramprasadvaral13@users.noreply.github.com>
2023-09-14 18:42:38 +0530

    add the files for code deoly

---
+++ scripts/startcontainer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker pull ramprasadv7/python-flask:latest
+
+
+# Run the Docker image as a container
+docker run -it -p 5000:5000 ramprasadv7/python-flask:latest
