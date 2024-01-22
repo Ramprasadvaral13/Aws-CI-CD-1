62b8a3e3a652377d6339a8200b06ea3a85770915
Ramprasadvaral13 <136104228+Ramprasadvaral13@users.noreply.github.com>
2024-01-22 19:43:36 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -6,4 +6,4 @@ docker pull ramprasadv7/test-build:latest
 
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 ramprasadv7/test-build:latest
+docker run -d -p 8000:8000 ramprasadv7/test-build:latest
