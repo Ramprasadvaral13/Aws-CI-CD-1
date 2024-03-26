0d372a3a8a1c82c189a1df5b9a23be6cdd302bdc
Ramprasadvaral13 <136104228+Ramprasadvaral13@users.noreply.github.com>
2024-03-26 14:00:01 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,8 +2,8 @@
 set -e 
 
 # Pull the Docker image from Docker Hub
-#docker pull ramprasadv7/test-build:latest
+docker pull ramprasadv7/test-build:latest
 
 
 # Run the Docker image as a container
-#docker run -d -p 8000:8000 ramprasadv7/test-build:latest
+docker run -d -p 8000:8000 ramprasadv7/test-build:latest
